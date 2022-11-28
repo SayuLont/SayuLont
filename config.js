48894ddbaccc75d00d2f5c65d077754537ed0107
SayuLont <99880401+SayuLont@users.noreply.github.com>
2022-11-28 12:05:14 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,79 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev && Papah-Chan
+   * Contact Me on wa.me/628989513496
+   * Follow https://github.com/FahriAdison
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6285215319934']
+global.ownernomer = "6285215319934"
+global.premium = ['6285215319934']
+global.packname = 'Sticker By'
+global.author = 'Ikyii-ads-bot'
+global.sessionName = 'papah'  //jangan diganti bro nanti error
+global.jumlha = '99'
+global.jumhal = '100'
+global.jumlah = '100'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+
+// Setting Mess
+global.mess = {
+    success: '✅Selesai',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Error!',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+
+// Fake
+global.thumb = fs.readFileSync('./media/image/papah.jpg')
+global.faall = fs.readFileSync('./media/image/menu.jpg')
+
+// Url
+global.mygit = 'https://github.com/rizkiadiasa'
+global.myyt = 'https://github.com/rizkiadiasa'
+global.myytv = ''
+global.mygc = ""
+
+// Engak Ngaruh, Belum Kepasang
+global.botname = 'Ikyii Ads'
+global.akulaku = 'Ikyii Ads'
+global.ytname = 'Ikyii Ads' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
